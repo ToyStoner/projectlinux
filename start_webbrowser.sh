0f090ff69760e5bfff5e2871f15bf14f017edab8
Meeremans Jasper <meeremans.jasper@gmail.com>
2025-01-16 22:04:09 +0100

    update

---
+++ start_webbrowser.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+sleep 20
+chromium-browser --start-fullscreen --kiosk --hide-scrollbars  /home/meere/projectlinux/projectlinux/index.html
+#python3 /home/meere/projectlinux/projectlinux/BLEvalsDatabase.py
+
